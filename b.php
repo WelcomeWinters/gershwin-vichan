9179a5187ef980226e7b296dc37e877d5fbdd740
Ira Gershwin <31814034+WelcomeWinters@users.noreply.github.com>
2023-02-25 08:45:10 -0500

    fixed banners on php 7.4

--- b.php
+++ b.php
@@ -5,13 +5,14 @@ $images = array_diff($files, array('.', '..'));
 $name = $images[array_rand($images)];
 // open the file in a binary mode
 $fp = fopen($dir . $name, 'rb');
+$fs = fstat($fp);
 
 // send the right headers
 header('Cache-Control: no-cache, no-store, must-revalidate'); // HTTP 1.1
 header('Pragma: no-cache'); // HTTP 1.0
 header('Expires: 0'); // Proxies
-header('Content-Type: ' . $fp['type']);
-header('Content-Length: ' . $fp['bytes']);
+header('Content-Type: ' . $fs['rdev']);
+header('Content-Length: ' . $fs['size']);
 
 // dump the picture and stop the script
 fpassthru($fp);
--- inc/instance-config.php
+++ inc/instance-config.php
@@ -1,23 +1,19 @@
 <?php
 
 /*
- *  Instance Configuration
- *  ----------------------
- *  Edit this file and not config.php for imageboard configuration.
- *
- *  You can copy values from config.php (defaults) and paste them here.
- */
+*  Instance Configuration
+*  ----------------------
+*  Edit this file and not config.php for imageboard configuration.
+*
+*  You can copy values from config.php (defaults) and paste them here.
+*/
 
 
+	$config['db']['server'] = 'localhost';
+	$config['db']['database'] = 'gershwin';
+	$config['db']['prefix'] = '';
+	$config['db']['user'] = 'lain';
+	$config['db']['password'] = 'LoudAnkleInnocentNine=';
 
-	// Database stuff
-	$config['db']['type']		= 'mysql';
-	$config['db']['server']		= 'localhost';
-	$config['db']['user']		= '';
-	$config['db']['password']	= '';
-	$config['db']['database']	= '';
-	
-	//$config['root']				= '/';
-	
-	@include('inc/secrets.php');
+        @include('inc/secrets.php');
 ?>
